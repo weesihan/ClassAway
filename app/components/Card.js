9c80e5148c90ec1070017f0c60425c927cab86fa
lethiciars <santosolethicia@gmail.com>
2021-06-18 13:28:38 +0800

    Added Card component

---
+++ app/components/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function Card(props) {
+    return (
+        <View style={styles.card}>
+
+            {props.children}
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white',
+        borderRadius: 8,
+        elevation: 3,
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        width: 275,
+        height: 275,
+        marginHorizontal: 4,
+        marginVertical: 6,
+    },
+    cardContent: {
+        marginHorizontal: 10,
+        marginVertical: 10,
+    },
+    titleText: {
+        fontFamily: 'Poppins-Bold',
+        fontSize: 16,
+        color: "black",
+    },
+    descText: {
+        fontFamily: 'Poppins-Light',
+        fontSize: 14,
+        color: "black",
+    },
+    cardImg: {
+        width: 275,
+        height: 150,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+    },
+    image: {
+        width: 300,
+        height: 150,
+    },
+})
--- app/screens/MyClasses.js
+++ app/screens/MyClasses.js
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, Vieww, SafeAreaView, ScrollView, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, ScrollView, Image, FlatList } from 'react-native';
 import firebase from '../database/firebase';
+import Card from '../components/Card.js'
 
 
 export default function MyClasses({ navigation }) {
-    // query for classes where admin == uid and date > current date
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection('Classes')
+            .where("admin", "==", firebase.auth().currentUser.email)
+            .get()
+            .then((snapshot) => {
+                console.log(classes);
+                snapshot.forEach((doc) => {
+                    console.log(doc.data());
+                    classes.push(doc.data());
+                    console.log(classes)
+                });
+
+            })
+            .catch((e) => console.log('Errors while downloading => ', e));
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.classText}>
                 My classes
             </Text>
+            <FlatList data={classes} renderItem={({ item }) => (
+                <View alignItems="center" justifyContent="center">
+                    <Card>
+                        <Image style={styles.cardImg} source={{ uri: item.pic }} />
+                        <View style={styles.cardContent}>
+                            <Text style={styles.titleText}>{item.title}</Text>
+                            <Text style={styles.descText}>{item.description}</Text>
+                        </View>
+                    </Card>
+
+                </View>
+            )}
+            />
         </SafeAreaView>
 
     );
@@ -32,7 +65,8 @@ const styles = StyleSheet.create({
     classText: {
         fontFamily: 'Poppins-Bold',
         fontSize: 30,
-        color: "black",
+        color: "#6559ff",
+        margin: 10,
     },
     titleText: {
         fontFamily: 'Poppins-Bold',
@@ -44,8 +78,18 @@ const styles = StyleSheet.create({
         fontSize: 14,
         color: "black",
     },
-    testImg: {
+    cardImg: {
         width: 275,
-        height: 120,
-    }
+        height: 150,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+    },
+    image: {
+        width: 300,
+        height: 150,
+    },
+    cardContent: {
+        marginHorizontal: 10,
+        marginVertical: 10,
+    },
 });
