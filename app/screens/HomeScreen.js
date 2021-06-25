7c4ef66c78ef7680888c3eccfe221db9ad707399
lethiciars <santosolethicia@gmail.com>
2021-06-25 13:08:42 +0800

    Updated user homescreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,33 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native';
-import { ScreenStackHeaderBackButtonImage } from 'react-native-screens';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Image, FlatList, RefreshControl } from 'react-native';
 import firebase from '../database/firebase';
+import Card from '../components/Card.js'
+import { set } from 'react-native-reanimated';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScreenStackHeaderBackButtonImage } from 'react-native-screens';
 
-export default function HomeScreen() {
-    const [imageUrl, setImageUrl] = useState(undefined);
 
-    useEffect(() => {
+export default function HomeScreen(props) {
+
+    const [classes, setClasses] = useState([])
+    const [isFetching, setFetching] = useState(true)
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        getData();
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+
+    const wait = timeout => {
+        return new Promise(resolve => {
+            setTimeout(resolve, timeout);
+        });
+    };
+
+    const cardSelected = async (id) => {
+        console.log("class pressed")
+        await props.navigation.navigate("ClassDetails", { id: id })
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+            <View alignItems="center" justifyContent="center">
+                <TouchableOpacity onPress={() => props.navigation.navigate("ClassDetails", { id: item.id })}>
+                    <Card>
+                        <Image style={styles.cardImg} source={{ uri: item.pic }} />
+                        <View style={styles.cardContent}>
+                            <Text style={styles.titleText}>{item.title}</Text>
+                            <Text style={styles.descText}>{item.description}</Text>
+                        </View>
+                    </Card>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    const getData = () => {
+        setFetching(true)
+        console.log(isFetching)
+        var tempClasses = []
         firebase
             .firestore()
             .collection('Classes')
-            .doc('TIi1azTXvSmzUD84wi4l')
             .get()
             .then((snapshot) => {
-                setImageUrl(snapshot.data().pic)
-            })
-            .catch((e) => console.log('Errors while downloading => ', e));
-      }, []);
-    
+                console.log(tempClasses);
+                snapshot.forEach((doc) => {
+                    let data = doc.data()
+                    data.id = doc.id
+                    console.log(doc.data());
+                    tempClasses.push(data);
+                    console.log(tempClasses)
+
+                });
+                setClasses(tempClasses)
+                setFetching(false)
+                console.log(classes)
+                console.log(isFetching)
+            }
+            )
+    }
+
+    useEffect(() => { getData() }, []);
+
 
     return (
-        <SafeAreaView style={styles.container}> 
-        <Image
-            style={styles.image}
-            source={{uri : imageUrl}}
-        />
-        <Text style={styles.classText}>
-            Welcome back {firebase.auth().currentUser.displayName}!
-        </Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.classText}>
+                Welcome back {firebase.auth().currentUser.displayName}!
+            </Text>
+            <FlatList
+                data={classes}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.title}
+                extraData={classes}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+            />
         </SafeAreaView>
     );
 }
@@ -35,14 +94,46 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#6559ff',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    containerScroll: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
     },
     classText: {
         fontFamily: 'Poppins-Bold',
         fontSize: 30,
+        color: "#6559ff",
+        margin: 10,
+    },
+    titleText: {
+        fontFamily: 'Poppins-Bold',
+        fontSize: 16,
         color: "black",
+    },
+    descText: {
+        fontFamily: 'Poppins-Light',
+        fontSize: 14,
+        color: "black",
+    },
+    cardImg: {
+        width: 275,
+        height: 150,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+    },
+    image: {
+        width: 300,
+        height: 150,
+    },
+    cardContent: {
+        marginHorizontal: 10,
+        marginVertical: 10,
     },
     image: {
         width: 300,
