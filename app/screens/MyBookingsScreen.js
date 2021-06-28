fc557e7749cdfbab7ba5fdfff1007c9b2709115c
lethiciars <santosolethicia@gmail.com>
2021-06-28 11:10:14 +0800

    Updated MyBookings screen

--- app/screens/MyBookingsScreen.js
+++ app/screens/MyBookingsScreen.js
@@ -1,24 +1,143 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Image, FlatList, RefreshControl } from 'react-native';
+import firebase from '../database/firebase';
+import { set } from 'react-native-reanimated';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
+export default function MyBookings(props) {
+    const [classes, setClasses] = useState([])
+    const [isFetching, setFetching] = useState(true)
+    const [refreshing, setRefreshing] = useState(false);
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        getData();
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
 
-export default function MyBookings({ navigation }) {
+    const wait = timeout => {
+        return new Promise(resolve => {
+            setTimeout(resolve, timeout);
+        });
+    };
+
+    const cardSelected = async (id) => {
+        console.log("class pressed")
+        await props.navigation.navigate("ClassDetails", { id: id })
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+            <View alignItems="center" justifyContent="center">
+                <TouchableOpacity onPress={() => props.navigation.navigate("ClassDetails", { id: item.id })}>
+                    <View style={styles.item}>
+                        <Text style={styles.titleText}>{item.title}</Text>
+                        <Text style={styles.descText}>{getDate(item.date)}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    const getDate = (date) => {
+        var t = new Date(date.seconds * 1000 + date.nanoseconds / 1000000);
+        var hours = t.getHours();
+        var minutes = t.getMinutes();
+        var newformat = t.getHours() >= 12 ? 'PM' : 'AM';
+
+        // Find current hour in AM-PM Format 
+        hours = hours % 12;
+
+        // To display "0" as "12" 
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var formatted =
+            (t.toString().split(' ')[0])
+            + ', ' + ('0' + t.getDate()).slice(-2)
+            + '/' + ('0' + (t.getMonth() + 1)).slice(-2)
+            + '/' + (t.getFullYear())
+            + ' - ' + ('0' + t.getHours()).slice(-2)
+            + ':' + ('0' + t.getMinutes()).slice(-2)
+            + ' ' + newformat;
+        return formatted
+    }
+
+    const getData = () => {
+        setFetching(true)
+        console.log(isFetching)
+        var tempClasses = []
+        firebase
+            .firestore()
+            .collection('Classes')
+            .get()
+            .then((snapshot) => {
+                console.log(tempClasses);
+                snapshot.forEach((doc) => {
+                    let data = doc.data()
+                    data.id = doc.id
+                    console.log(doc.data());
+                    tempClasses.push(data);
+                    console.log(tempClasses)
+
+                });
+                setClasses(tempClasses)
+                setFetching(false)
+                console.log(classes)
+                console.log(isFetching)
+            }
+            )
+    }
+
+    useEffect(() => { getData() }, []);
+
 
     return (
         <SafeAreaView style={styles.container}>
-        <Text style={styles.classText}>
-            My Bookings!
-        </Text>
+            <Text style={styles.classText}>
+                Welcome back {firebase.auth().currentUser.displayName}!
+            </Text>
+            <FlatList
+                data={classes}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.title}
+                extraData={classes}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+            />
         </SafeAreaView>
     );
 }
 
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#6559ff',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    item: {
+        backgroundColor: 'white',
+        borderRadius: 8,
+        elevation: 3,
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        width: 275,
+        height: 100,
+        marginHorizontal: 4,
+        marginVertical: 9,
+        padding: 6,
+    },
+    titleText: {
+        fontFamily: 'Poppins-Bold',
+        fontSize: 16,
+        color: "black",
+    },
+    descText: {
+        fontFamily: 'Poppins-Light',
+        fontSize: 14,
+        color: "black",
     },
     classText: {
         fontFamily: 'Poppins-Bold',
